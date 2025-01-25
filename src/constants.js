fcf10ccc71cfc8ba714961c393eacb6ef6b240b3
axceee <akshit14022007@gmail.com>
2025-01-25 10:44:02 +0530

    e1

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+const DB_NAME = 'e'
+export default DB_NAME;
---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import DB_NAME from '../constants.js';
+dotenv.config()
+const MONGO_URL=process.env.MONGO_URL
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
+        console.log('db connected good job :] ')
+    } 
+    catch(error){
+        console.error(`you stupid this thing is wrong, look -> ${error}`);
+        process.exit()
+    }
+}
+export default connectDB
