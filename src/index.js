d8bd65b1c1bb8db8883fb82bee5e8f53ba8cb823
axceee <akshit14022007@gmail.com>
2025-01-27 10:37:28 +0530

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import express from 'express'
 import dotenv from 'dotenv'
+import express from 'express'
+dotenv.config()
+const port = process.env.PORT || 3000 
 import connectDB from './db/index.js'
+const app = express()
 
 connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`port is ${port}`)
+    })
+})
+.catch((err)=>{
+    console.log(`connection failed :( ${err}`)
+})
